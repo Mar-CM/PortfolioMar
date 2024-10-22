70f381c6ad038efb454523fb71f0c366618a800a
Mar Caballero Moryno <mar.caballero@hotmail.es>
2024-10-22 23:49:37 +0200

    changes estructure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
 import "../scss/App.scss";
 
 import Header from "./Header.jsx";
-import Main from "./Main.jsx"
+import Main from "./Main.jsx";
 
 function App() {
 	return (
 		<>
 			<Header />
-			<body>
-				<Main />
-			</body>
+			<Main />
 		</>
 	);
 }
