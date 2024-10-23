ae902315bbd2ca0a805ae84489e05e878acf8e14
Mar Caballero Moryno <mar.caballero@hotmail.es>
2024-10-24 00:07:59 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
 	return (
 		<footer>
 			<div className="containerFooter">
-				<span> © 2024 Portfolio | Mar Caballero</span>
+				<span> © 2024 Portfolio | Mar Caballero ❤</span>
 			</div>
 		</footer>
 	);
