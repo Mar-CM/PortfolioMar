0a80556087818810612883d3b3ca0223a2f54522
Mar Caballero Moryno <mar.caballero@hotmail.es>
2024-10-24 21:06:00 +0200

    Section about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,35 @@
 function AboutMe() {
 	return (
 		<>
-			<section className="aboutme">
-				<h2>Sobre mí</h2>
+			<section className="aboutMe">
+				<h3 className="aboutMe_tittle">Sobre mí</h3>
+				<p className="aboutMe_text">
+					¡Hola mundo! 🌞 Soy Mar, una programadora Junior especializada en
+					Front-end con conocimientos en Back-end. A lo largo de mi vida, he
+					trabajado en el mundo de la sanidad, donde desarrollé una fuerte
+					capacidad para la atención al cliente y un don de gentes que me ha
+					permitido crear un ambiente agradable y profesional.
+				</p>
+				<p className="aboutMe_text">
+					Me siento afortunada de haber encontrado mi pasión en el desarrollo
+					web, lo que me permite transformar ideas en interfaces dinámicas y
+					funcionales. En especial, disfruto trabajar con React JS, una
+					herramienta que me ayuda a mejorar la eficiencia en la interacción con
+					productos digitales.
+				</p>
+				<p className="aboutMe_text">
+					En mi último trabajo, estuve mano a mano con la parte tech, lo que me
+					ha impulsado a hacer un cambio en mi vida hacia el desarrollo web. Me
+					motiva colaborar con equipos multidisciplinarios para llevar proyectos
+					de alta calidad desde el concepto hasta la implementación final. Soy
+					una persona dinámica y resolutiva, siempre dispuesta a superar
+					expectativas, adaptarme a las necesidades del entorno y aportar valor
+					a cada proyecto en el que participo.
+				</p>
+				<h4>
+					¡Estoy lista para dar lo mejor de mí y contribuir al éxito de tu
+					equipo! 😜
+				</h4>
 			</section>
 		</>
 	);
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+	return (
+		<>
+			<section className="contact">
+				<h2>Contacto</h2>
+			</section>
+		</>
+	);
+}
+
+export default Contact;
