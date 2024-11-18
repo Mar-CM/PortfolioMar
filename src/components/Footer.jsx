d20af7acafca43e654075f7c46dece8bfea14973
Mar Caballero Moryno <mar.caballero@hotmail.es>
2024-11-18 20:55:28 +0100

    Change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
+import linkedin from "../images/linkedin.svg";
+import github from "../images/github.svg";
+
 function Footer() {
 	return (
 		<footer>
 			<div className="containerFooter">
-				<span> © 2024 Portfolio | Mar Caballero ❤</span>
+				<div className="containerFooter_Icons">
+					<a
+						target="_blank"
+						href="https://www.linkedin.com/in/mar-caballero-moryno-511398204/"
+					>
+						<img src={linkedin} alt="Linkedin icon" className="footerIcon" />
+					</a>
+					<a target="_blank" href="https://github.com/Mar-CM">
+						<img src={github} alt="Github icon" className="footerIcon" />
+					</a>
+				</div>
+				<span>© 2024 Portfolio | Mar Caballero</span>
 			</div>
 		</footer>
 	);
